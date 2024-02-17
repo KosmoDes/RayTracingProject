097c88a3d4b38702cb0463396cd6b79c28fcc63a
Ilya Sviridov <ilya.sviridov.2007@mail.ru>
2024-02-17 13:03:37 +0300

    edit project structure

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include "Tracing.h"
+
+int main(){
+    int h, w, d;
+    std::cin >> w >> h >> d;
+    Canvas c{h, w};
+    Scene scene(std::vector<Object*>{new Sphere(2, Vector3{2.4, 4, 8}, Vector3{255, 223, 253})});
+    Vector3 O{0, 0, 0};
+    Camera cam(d, O);
+    for(auto x : range(0, Cw + 1)){
+        for(auto y : range(0, Ch + 1)){
+            Vector3 D = cam.CanvasToViewPort(x, y);
+            double color = cam.TraceRay(O, D, d, inf, scene);
+            c.setPoint(x, y, color);
+        }
+    }
+    c.show();
+    c.next_frame();
+    scene._objects[0]->coords[1]++;
+    for(auto x : range(0, Cw + 1)){
+        for(auto y : range(0, Ch + 1)){
+            Vector3 D = cam.CanvasToViewPort(x, y);
+            double color = cam.TraceRay(O, D, d, inf, scene);
+            c.setPoint(x, y, color);
+        }
+    }
+    c.show();
+    c.next_frame();
+    system("pause");
+}
